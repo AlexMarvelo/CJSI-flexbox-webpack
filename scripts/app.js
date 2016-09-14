fb9803a09fa7f92c2ed6d506a01ecb3163ebb5aa
AlexMarvelo <mironenkoalexey@gmail.com>
2016-09-14 17:07:12 +0300

    disable pug

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,6 @@
 var normalize = require('../node_modules/normalize.css/normalize.css'),
     styles = require('../styles/main.sass');
-    // pug = require('../node_modules/pug');
+// var pug = require('../node_modules/pug');
 var gallery = require('../components/gallery/gallery.js')
 
 var app = {};
